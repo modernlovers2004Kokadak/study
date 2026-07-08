fe466309498970976368b7b58a68fa2d49342c58
modernlovers2004Kokadak <modernlovers2004@gmail.com>
2026-07-08 15:23:34 +0900

    全ファイル削除

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='riyoshi-integrated-v2-1-0';
-const ASSETS=['./','./index.html','./style.css','./manifest.webmanifest','./教材/index.html','./過去問/index.html'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return res;})).catch(()=>caches.match('./index.html')));});
--- 過去問/count.js
+++
@@ -1,2 +0,0 @@
-require('vm').runInThisContext(require('fs').readFileSync('/mnt/data/work_kakomon/data.js','utf8'));
-for (const c of CATEGORIES){console.log(c.id,c.name, QUESTIONS.filter(q=>q.category===c.id).length)}
