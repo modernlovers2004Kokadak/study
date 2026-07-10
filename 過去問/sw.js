5b96d4435e9dce533c38ed6f93296458d40ce309
modernlovers2004Kokadak <modernlovers2004@gmail.com>
2026-07-10 13:55:13 +0900

    Add files via upload

--- 過去問/sw.js
+++ 過去問/sw.js
@@ -3,19 +3,23 @@ const CACHE_PREFIX='riyo-kakomon-';
 const ASSETS=['./','./index.html','./style.css','./data.js','./app.js','./manifest.webmanifest'];
 
 self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c=>c.addAll(ASSETS))
+      .then(()=>self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate',e=>{
   e.waitUntil(
-    caches.keys().then(keys=>Promise.all(
-      keys
-        .filter(k=>k.startsWith(CACHE_PREFIX)&&k!==CACHE)
-        .map(k=>caches.delete(k))
-    ))
+    caches.keys()
+      .then(keys=>Promise.all(
+        keys
+          .filter(k=>k.startsWith(CACHE_PREFIX)&&k!==CACHE)
+          .map(k=>caches.delete(k))
+      ))
+      .then(()=>self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch',e=>{
