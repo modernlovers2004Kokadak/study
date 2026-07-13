4f74d7b6cacc3f255059e326812f8538b49f1d29
modernlovers2004Kokadak <modernlovers2004@gmail.com>
2026-07-13 21:43:06 +0900

    Add files via upload

--- 過去問/sw.js
+++ 過去問/sw.js
@@ -1,4 +1,4 @@
-const CACHE='riyoushi-kakomon-v2983';
+const CACHE='riyoushi-kakomon-v2984';
 const CACHE_PREFIX='riyo-kakomon-';
 const ASSETS=['./','./index.html','./style.css','./data.js','./app.js','./manifest.webmanifest'];
 
